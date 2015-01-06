abde876b382ec3f660f730dd92d89855225d623d
koty <kouichi.nishizawa+github@gmail.com>
2015-01-06 15:09:32 +0900

    mysql-connectorを使ってみる

--- mybook/settings.py
+++ mybook/settings.py
@@ -59,9 +59,15 @@ WSGI_APPLICATION = 'mybook.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mybook',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
